02567b844ca9eaffb1b3acc6908fc5a14c326c59
sn2979 <sn2979@princeton.edu>
2023-08-01 19:11:34 -0400

    completed objectives of charting ratio and triggers

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,36 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_A: number,
+  price_B: number,
+  lower_bound: number,
+  upper_bound: number,
+  ratio: number,
+  trigger_alert: number | undefined,
   timestamp: Date,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+  static generateRow(serverResponds: ServerRespond[]) : Row {
+      const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+      const priceDEF = (serverResponds[1].top_ask.price + serverResponds[0].top_bid.price) / 2;
+
+      const raTio = priceABC/priceDEF;
+      const upperBound = 1.1;
+      const lowerBound = 0.99;
+      const timeStamp = (serverResponds[0].timestamp > serverResponds[1].timestamp) ?
+                        serverResponds[0].timestamp : serverResponds[1].timestamp;
+      const triggerAlert = (raTio > upperBound || raTio < lowerBound) ? raTio : undefined;
+
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        price_A: priceABC,
+        price_B: priceDEF,
+        ratio: raTio,
+        upper_bound: upperBound,
+        lower_bound: lowerBound,
+        trigger_alert: triggerAlert,
+        timestamp: timeStamp,
       };
-    })
   }
 }
